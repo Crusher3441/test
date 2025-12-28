0fe75182bf5229e67fd6a8b1556618530eb8d93a
Crusher3441 <vishal.singhh.2003@gmail.com>
2025-12-28 14:02:43 +0530

    singers added

--- test-react/src/App.jsx
+++ test-react/src/App.jsx
@@ -10,22 +10,25 @@ function App() {
     <>
       <h1>Github tutorial</h1>
       <h2>changes by vishal</h2>
-      {/* Lorem ipsum, dolor sit amet consectetur adipisicing eli
       
-      t. Doloribus facilis sapiente ratione explicabo assumenda quo deleniti dolorem nisi quis ad. Dolore sapiente ducimus eaque voluptas quas neque mollitia quia totam perspiciatis odio, voluptates repellendus officiis quod est aliquam unde eveniet nobis consectetur? Animi quibusdam recusandae consequatur eos quaerat ab consequuntur iste sequi, re
-      
-      pellendus, maxime nam a rem sit accusamus facilis esse qu
-      
-      asi culpa eum tempore facere eius dignissimos illum soluta ducimus! Neque sunt quaerat, vel qui modi voluptatum alias doloremque dolorum sint, et excepturi autem ratione, iure consectetur. */}
 
 
-      {/* changes by aryan */}
+      <h4>changes by aryan</h4>
 
       <p> top 3 actors : </p>
       <ul>
         <li> john cena </li>
         <li> sunny deol </li>
         <li> iliana </li>
+      </ul>
+
+
+      <h4>changes by vishal</h4>
+      <p> top 3 singers : </p>
+      <ul>
+        <li> john sachdeva </li>
+        <li> sunny singh </li>
+        <li> honny kakkar </li>
       </ul>
 
 
